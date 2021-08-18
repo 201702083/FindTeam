fa2747570fd6ef1d2c26a2115f2ab20b53899488
201702083 <201702083@o.cnu.ac.kr>
2021-08-18 21:14:10 +0900

    Sign, SignIn layout and submit event

--- ui/src/views/Sign.vue
+++ ui/src/views/Sign.vue
@@ -1,5 +1,29 @@
 <template>
-  <div >
-    <h1>This is an sign page</h1>
-  </div>
+  <form @submit="onSubmit">
+    <div class="form-group mt-5">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control mt-3" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group mt-5">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control mt-3" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-primary mt-5">Submit</button>
+  </form>
 </template>
+
+<script>
+export default {
+  setup() {
+    
+
+  },
+  methods:{
+    onSubmit(event){
+      event.preventDefault();
+      // get 후 response 성공하면 쿠키 생성 후 home/status로
+      this.$router.push('/home/status')
+    }
+  }
+}
+</script>
