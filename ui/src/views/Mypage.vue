33267618f53b520fd5274bd78118a2dfa707f78e
201702083 <201702083@o.cnu.ac.kr>
2021-08-30 16:00:53 +0900

    Mypage information grid set

--- ui/src/views/Mypage.vue
+++ ui/src/views/Mypage.vue
@@ -1,5 +1,21 @@
 <template>
-  <div >
-    <h1>This is an Mypage</h1>
-  </div>
+  <b-container fluid>
+    <b-card>
+      <b-row>
+        <b-col class="col-sm-5">
+          <img src="../assets/logo.png" width=100% height=100%/>
+        </b-col>
+        <b-col class="col-sm">
+          info
+        </b-col>
+      </b-row>
+    </b-card>
+
+  </b-container>
 </template>
+
+<style>
+*{
+  border: solid 1px;
+}
+</style>
