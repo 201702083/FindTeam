a76e4472e2f3494ef5a00a655a3d8a5316f6d2ae
201702083 <201702083@o.cnu.ac.kr>
2021-11-10 23:37:26 +0900

    axios post api done

--- ui/src/views/Sign.vue
+++ ui/src/views/Sign.vue
@@ -2,12 +2,12 @@
   <form @submit="onSubmit" >
    
     <div class="form-group mt-5">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control mt-3" id="Email" aria-describedby="emailHelp" placeholder="Enter email">
+      <label for="exampleInputEmail1">ID</label>
+      <input v-model="id"  class="form-control mt-3" id="Email" aria-describedby="emailHelp" placeholder="Enter email">
     </div>
     <div class="form-group mt-5">
       <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control mt-3" id="exampleInputPassword1" placeholder="Password">
+      <input v-model="pw" type="Password" class="form-control mt-3" id="exampleInputPassword1" placeholder="Password">
     </div>
     <button type="submit" class="btn btn-outline-primary m-5">Sign in</button>
     <div>
@@ -20,17 +20,38 @@
 
 <script>
 import {Got_Cookie} from "./Home.vue";
+import axios from "axios";
 Got_Cookie;
 export default {
   setup() {
     
 
   },
+  data(){
+    return {
+      id:null,
+      pw:null,
+    }
+  },
   methods:{
     onSubmit(event){
       event.preventDefault();
+      var requestBody = {
+        id : this.id,
+        pw : this.pw,
+      }
+      axios.post('/api/User/login' , requestBody).then(res => {
+        // axios.get('/api/db').then(res => {
+        console.log(res.data)
+        if(res.data.error === null){
+          console.log("data is null")
+        }
+        else{
+          console.log("Error")
+        }
+      })
       // get 후 response 성공하면 쿠키 생성 후 home/status로
-      this.$router.push('/home/status')
+      // this.$router.push('/home/status')
       this.Got_Cookie = true;
     }
   }
